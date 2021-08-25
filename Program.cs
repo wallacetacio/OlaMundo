0bf085178396993131446092813fa6a7161e7948
Wallace Tácio <wallacett.dev@gmail.com>
2021-08-24 21:45:13 -0300

    Adiciona o programa em C#: Olá mundo!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário, eu me chamo Wallace!");
+        }
+    }
+}
